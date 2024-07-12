dd45bc09d534e46d90330fae72ed6f313b4084bc
abijith r <93731985+geekeeeee@users.noreply.github.com>
2024-07-12 19:08:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
         description="A discord bot developed using python and discord developer API , which supports 2-Player games and could fetch weather using weather API services. Learned some new ideas on creating game instances and managing these instances when a user reacts to the bot's message." 
         link="https://discord.com/oauth2/authorize?client_id=1247624200655601686&permissions=843961281600&scope=bot"/>
         <Projectreveal1 projectName="ShadowLeet" 
-        description=" A web extension that enables a dark mode for leetcode contest page using CSS injection using jQuery and chrome developer API."
+        description=" A web extension that enables a dark mode for leetcode contest page using CSS injection with jQuery and chrome developer API."
         link="https://github.com/geekeeeee/ShadowLeet"/>
         <Projectreveal2 projectName="MovieBase App" 
         description="A project developed as part of our DBMS course curriculum. Spearheaded the frontend part for this project. Stunning Frontend UI created using ReactJS and taiwindCSS." 
